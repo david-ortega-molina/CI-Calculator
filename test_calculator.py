ff9fce99b3b4948b17a9394102f669d45fcb635a
david-ortega-molina <63212572+david-ortega-molina@users.noreply.github.com>
2021-06-06 22:34:07 -0700

    added test

---
+++ test_calculator.py
@@ -0,0 +1,14 @@
+"""
+Test for calc app
+"""
+import calculator
+
+
+class TestCalculatorApp:
+    def test_add(self):
+        assert 5 == calculator.add(3, 2)
+
+    def test_sub(self):
+        assert 5 == calculator.sub(10, 5)
+
+    
