4f1cf3705a02d2e4e974197c2926bb6e26ecc92a
david-ortega-molina <63212572+david-ortega-molina@users.noreply.github.com>
2021-06-06 23:24:57 -0700

    added test mult and div

--- test_calculator.py
+++ test_calculator.py
@@ -11,4 +11,10 @@ class TestCalculatorApp:
     def test_sub(self):
         assert 5 == calculator.sub(10, 5)
 
+    def test_mult(self):
+        assert 10 == calculator.sub(2, 5)
+
+    def test_div(self):
+        assert 6 == calculator.sub(3, 2)
+
     
